4c60ff97f7ebd2aa1e3cb37075185b2f3e645f64
Mick van Gelderen <mickvangelderen@gmail.com>
2015-08-31 19:49:05 +0200

    Added test cases for is

---
+++ test/types.js
@@ -0,0 +1,57 @@
+module.exports = [
+	{
+		value: true,
+		name: 'true'
+	},{
+		value: false,
+		name: 'false'
+	},{
+		value: null,
+		name: 'null'
+	},{
+		value: undefined,
+		name: 'undefined'
+	},{
+		value: 1,
+		name: '1'
+	},{
+		value: -1,
+		name: '-1'
+	},{
+		value: 0,
+		name: '0'
+	},{
+		value: -0,
+		name: '-0'
+	},{
+		value: -Infinity,
+		name: '-Infinity'
+	},{
+		value: +Infinity,
+		name: '+Infinity'
+	},{
+		value: NaN,
+		name: 'NaN'
+	},{
+		value: function() {},
+		name: 'function() {}',
+		isFunction: true
+	},{
+		value: {},
+		name: '{}',
+		isObject: true
+	},{
+		value: new Date(),
+		name: 'new Date()'
+	},{
+		value: [],
+		name: '[]',
+		isArray: true
+	},{
+		value: '',
+		name: ''
+	},{
+		value: 'string',
+		name: 'string'
+	}
+]
