fdc0bbca0f4283e653de548519b6c99ba41c9bd5
Mick van Gelderen <mickvangelderen@gmail.com>
2015-09-01 10:53:15 +0200

    Moved clone and each to adhere-common

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 var adhere = module.exports = require('./lib/adhere');
 
-adhere.clone = require('./lib/clone');
-adhere.each = require('./lib/each');
-adhere.pipe = require('./lib/pipe');
 adhere.is = require('./lib/is');
+adhere.pipe = require('./lib/pipe');
